b420d3de4f1a0062dab582978321559491a110a6
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-03-27 15:28:19 +0000

    - Prepped for 6.4.0 Final

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'dns';
-$app['version'] = '1.4.14';
+$app['version'] = '1.4.30';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-dns.spec
+++ packaging/app-dns.spec
@@ -1,7 +1,7 @@
 
 Name: app-dns
 Epoch: 1
-Version: 1.4.14
+Version: 1.4.30
 Release: 1%{dist}
 Summary: DNS Server
 License: GPLv3
