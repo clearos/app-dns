e0cfb00aa30d2cff49eca20a6fd1098825ac9752
clearostranslations <translations@clearfoundation.com>
2014-11-21 19:36:44 -0500

    Updated translations via Pootle

--- language/ru_RU/dns_lang.php
+++ language/ru_RU/dns_lang.php
@@ -3,7 +3,7 @@
 // http://www.clearfoundation.com/docs/developer/translations/
 
 $lang['dns_alias'] = 'Alias';
-$lang['dns_app_description'] = 'The local DNS server can be used for mapping IP addresses on your network to hostnames.';
-$lang['dns_app_name'] = 'DNS Server';
-$lang['dns_dns_entry'] = 'DNS Entry';
-$lang['dns_dns_server'] = 'DNS Server';
+$lang['dns_app_description'] = 'Локальный сервер DNS может использоваться для отображения IP-адресов вашей сети и имени узлов.';
+$lang['dns_app_name'] = 'DNS Сервер';
+$lang['dns_dns_entry'] = 'DNS Запись';
+$lang['dns_dns_server'] = 'DNS Сервер';
