edbc3c1b0d0c75b66dc274af7c3ae5edbe60f29f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:49:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'La loka DNS servilo povas esti uzata por mapado IP adresoj sur via reto por hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Eniro';
+$lang['dns_dns_server'] = 'DNS Server';
