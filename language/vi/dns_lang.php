f51fabc956c4ea9232467bf9f6034a191f7fe6b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:59:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'tên giả';
+$lang['dns_app_description'] = 'Các máy chủ DNS địa phương có thể được sử dụng cho các địa chỉ IP trên bản đồ mạng của bạn để hostname.';
+$lang['dns_app_name'] = 'DNS server';
+$lang['dns_dns_entry'] = 'DNS nhập';
+$lang['dns_dns_server'] = 'DNS server';
