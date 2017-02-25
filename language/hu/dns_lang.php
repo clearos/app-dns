20993624a4db15d568d7297c10e8eb7de88e0e45
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:51:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'álnév';
+$lang['dns_app_description'] = 'A helyi DNS szerver lehet használni az IP-címek a hálózaton nevekre.';
+$lang['dns_app_name'] = 'DNS-kiszolgáló';
+$lang['dns_dns_entry'] = 'DNS bejegyzés';
+$lang['dns_dns_server'] = 'DNS-kiszolgáló';
