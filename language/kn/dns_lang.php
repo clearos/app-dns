a8b8009bcde0815526a3a113f12076dde4cfc44a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:52:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ಅಲಿಯಾಸ್';
+$lang['dns_app_description'] = 'ಸ್ಥಳೀಯ DNS ಸರ್ವರ್ ಸಂಕುಲ ಹೆಸರು ನಿಮ್ಮ ಜಾಲಬಂಧದಲ್ಲಿ ಮ್ಯಾಪಿಂಗ್ IP ವಿಳಾಸಗಳನ್ನು ಬಳಸಬಹುದು.';
+$lang['dns_app_name'] = 'DNS ಸರ್ವರ್';
+$lang['dns_dns_entry'] = 'ಡಿಎನ್ಎಸ್ ಎಂಟ್ರಿ';
+$lang['dns_dns_server'] = 'DNS ಸರ್ವರ್';
