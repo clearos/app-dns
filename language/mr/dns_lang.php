fe59b59587195efca40697cfbbb868aecd91e7bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:54:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ऊर्फ';
+$lang['dns_app_description'] = 'स्थानिक DNS सर्व्हर होस्ट नेटवक मॅपिंग IP पत्ते वापरले जाऊ शकते.';
+$lang['dns_app_name'] = 'DNS सर्व्हर';
+$lang['dns_dns_entry'] = 'DNS प्रविष्टी';
+$lang['dns_dns_server'] = 'DNS सर्व्हर';
