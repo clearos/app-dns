f2626d6cc2ebbcb74e329e87414e16b74a5992ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:59:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'عرف';
+$lang['dns_app_description'] = 'مقامی DNS سرور hostnames کے لئے آپ کے نیٹ ورک پر تعریفیں IP پتوں کے لئے استعمال کیا جا سکتا.';
+$lang['dns_app_name'] = 'DNS سرور';
+$lang['dns_dns_entry'] = 'DNS انٹری';
+$lang['dns_dns_server'] = 'DNS سرور';
