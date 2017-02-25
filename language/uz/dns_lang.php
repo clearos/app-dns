3c153da8f5a6d94e0511df0b167157c8fd21be68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:59:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'afsus';
+$lang['dns_app_description'] = 'mahalliy DNS server xost nomi uchun tarmoq bo&#39;yicha xaritalash IP manzillar uchun foydalanish mumkin.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
