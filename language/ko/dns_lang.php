c94bc9bc0a388ca39de15a0aef8fadeb443ebe06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:53:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = '별명';
+$lang['dns_app_description'] = '로컬 DNS 서버는 호스트 이름을 네트워크에 매핑 IP 주소를 사용할 수 있습니다.';
+$lang['dns_app_name'] = 'DNS 서버';
+$lang['dns_dns_entry'] = 'DNS 항목';
+$lang['dns_dns_server'] = 'DNS 서버';
