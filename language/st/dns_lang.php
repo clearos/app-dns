3e984e9996294d6bd5d987b8f1071e534690813e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:57:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Ba sebakeng seo DNS seva e ka sebediswa bakeng sa liaterese tseba likarolo IP tsa leqhubu ba hao hore ba hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
