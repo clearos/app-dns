f6766bfcdcb234a47703708fe94d903c3a5352b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:50:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ψευδώνυμο';
+$lang['dns_app_description'] = 'Ο τοπικός διακομιστής DNS μπορεί να χρησιμοποιηθεί για την χαρτογράφηση διευθύνσεων IP στο δίκτυό σας σε ονόματα εξυπηρετητών.';
+$lang['dns_app_name'] = 'διακομιστή DNS';
+$lang['dns_dns_entry'] = 'DNS Έναρξη';
+$lang['dns_dns_server'] = 'διακομιστή DNS';
