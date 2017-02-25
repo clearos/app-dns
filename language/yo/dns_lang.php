31b5134bbae258e39ea57e154d5acc6664a3f63e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:00:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'inagijẹ';
+$lang['dns_app_description'] = 'Awọn agbegbe DNS server le ṣee lo fun aworan IP adirẹsi on nẹtiwọki rẹ to hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS titẹsi';
+$lang['dns_dns_server'] = 'DNS Server';
