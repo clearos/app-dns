654b405d902b3981b24982bacc437d5f62dad69b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:55:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Den lokale DNS-serveren kan brukes til kartlegging av IP-adresser i nettverket til vertsnavn.';
+$lang['dns_app_name'] = 'DNS-server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS-server';
