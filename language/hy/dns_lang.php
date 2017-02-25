b15d9b583fa01da6b474a4bcaa6657e53ecd53da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:47:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'Alias';
+$lang['dns_app_description'] = 'Տեղական DNS սերվերը կարող է օգտագործվել քարտեզագրման IP հասցեներից ձեր ցանցի hostnames:';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
