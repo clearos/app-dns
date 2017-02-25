2f55ca6be505a4a2084d6fb1b749b75256d446ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:47:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ləqəb';
+$lang['dns_app_description'] = 'yerli DNS server ana kompüter adları üçün şəbəkə mapping IP ünvanlar üçün istifadə edilə bilər.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Giriş';
+$lang['dns_dns_server'] = 'DNS Server';
