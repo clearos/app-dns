8d2d23f863b7c5fa6b1771d51d53c7ac9d5b40c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:52:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ат';
+$lang['dns_app_description'] = 'жергілікті DNS сервері хост үшін желідегі картаға IP мекенжайлары үшін пайдаланылатын болады.';
+$lang['dns_app_name'] = 'DNS-сервер';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS-сервер';
