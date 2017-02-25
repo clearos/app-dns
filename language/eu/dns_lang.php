c6bed09eda250454a0c1f6db40b56f7055f27625
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:47:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Tokiko DNS zerbitzari mapping IP helbideak erabil daiteke zure sare ostalariak orrian.';
+$lang['dns_app_name'] = 'DNS zerbitzaria';
+$lang['dns_dns_entry'] = 'DNS sarrera';
+$lang['dns_dns_server'] = 'DNS zerbitzaria';
