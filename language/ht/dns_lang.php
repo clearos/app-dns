7f22c30ea35f7aa3b9ba19e257248beb31253721
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:50:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'jaden sa';
+$lang['dns_app_description'] = 'ka sèvè a dns lokal gen pou itilize pou adrès IP kat sou rezo ou a ostnam.';
+$lang['dns_app_name'] = 'dns sèvè';
+$lang['dns_dns_entry'] = 'dns Antre';
+$lang['dns_dns_server'] = 'dns sèvè';
