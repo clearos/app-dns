6f48331a54a7588d599e4b8604ac6b6c672800b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:47:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'الاسم المستعار';
+$lang['dns_app_description'] = 'يمكن استخدام ملقم DNS المحلي لعناوين بروتوكول الإنترنت رسم الخرائط على الشبكة للمضيفين.';
+$lang['dns_app_name'] = 'خادم DNS';
+$lang['dns_dns_entry'] = 'إدخال DNS';
+$lang['dns_dns_server'] = 'خادم DNS';
