5cf91c0988ef176df9dd038b83bd25e56574a570
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:48:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Akumeneko DNS makina angagwiritsidwe ntchito maadiresi sanjira IP pa Intaneti anu hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS kulowa';
+$lang['dns_dns_server'] = 'DNS Server';
