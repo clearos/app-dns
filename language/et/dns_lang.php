e27e9fdeecca62ee0e14d1e593dfe0431293f80b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:49:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Kohalik nimeserver saab kasutada kaardistamise IP aadresse võrgus hostinimedeks.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS kirje';
+$lang['dns_dns_server'] = 'DNS Server';
