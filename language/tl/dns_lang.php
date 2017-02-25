c0d4bcd4273f817fb1db0624e4c1cc78914caa74
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:58:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'palayaw';
+$lang['dns_app_description'] = 'Ang mga lokal na DNS server ay maaaring gamitin para sa pagmamapa ng mga IP address sa iyong network sa mga hostname.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
