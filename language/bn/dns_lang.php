53c708cabfea60c333a4f58a2174d3dceed17f0c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:47:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'উপনাম';
+$lang['dns_app_description'] = 'স্থানীয় DNS সার্ভার হোস্ট-নেম আপনার নেটওয়ার্কে ম্যাপিং আইপি ঠিকানার জন্য ব্যবহার করা যেতে পারে.';
+$lang['dns_app_name'] = 'DNS সার্ভার';
+$lang['dns_dns_entry'] = 'ডিএনএস এণ্ট্রি';
+$lang['dns_dns_server'] = 'DNS সার্ভার';
