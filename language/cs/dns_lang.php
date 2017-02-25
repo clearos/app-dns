4e3d4a4a3814c553050e5da1416b56e880a2fdd0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:49:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'přezdívka';
+$lang['dns_app_description'] = 'Místní server DNS můžete využít k mapování IP adres v síti na jména.';
+$lang['dns_app_name'] = 'DNS server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS server';
