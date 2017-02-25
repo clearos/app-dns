303136b9382ca0ce87fbf089d2401d58215b06f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:54:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'Өөр хаягийн';
+$lang['dns_app_description'] = 'Орон нутгийн DNS серверийн хост нэр рүү Хэрэв таны сүлжээний зураглалын IP хаяг ашиглаж болно.';
+$lang['dns_app_name'] = 'DNS сервер';
+$lang['dns_dns_entry'] = 'DNS төгөлдөр болох';
+$lang['dns_dns_server'] = 'DNS сервер';
