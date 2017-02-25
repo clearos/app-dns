59e8e56ef4640892c9dfe984b0688812be2f3555
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:53:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'kitaip';
+$lang['dns_app_description'] = 'Vietos DNS serveris gali būti naudojamas žemėlapių IP adresai tinkle į hostname&#39;ą.';
+$lang['dns_app_name'] = 'DNS serveris';
+$lang['dns_dns_entry'] = 'DNS įrašas';
+$lang['dns_dns_server'] = 'DNS serveris';
