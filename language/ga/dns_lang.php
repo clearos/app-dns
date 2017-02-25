c1fc247813d327939776a37d51a0338a4530b67c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:51:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'réimse';
+$lang['dns_app_description'] = 'Is féidir leis an freastalaí DNS áitiúil a úsáid le haghaidh seoltaí mapála IP ar do líonra a chur ar óstríomhaire.';
+$lang['dns_app_name'] = 'Freastalaí DNS';
+$lang['dns_dns_entry'] = 'DNS Iontráil';
+$lang['dns_dns_server'] = 'Freastalaí DNS';
