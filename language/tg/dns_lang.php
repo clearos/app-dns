e8d8c0385e8e77fb3471d8ef164eb0609cda6b52
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:58:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'номи кӯтоҳшуда';
+$lang['dns_app_description'] = 'Сервер DNS маҳаллӣ метавонанд барои нишониҳои харитасозии IP шабакаи шумо ба hostnames истифода бурда мешавад.';
+$lang['dns_app_name'] = 'DNS сервер';
+$lang['dns_dns_entry'] = 'Дохилшавӣ DNS';
+$lang['dns_dns_server'] = 'DNS сервер';
