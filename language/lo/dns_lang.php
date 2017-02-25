30ee473b42a2b8f6e2df6b02ae78503f8cbf64fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:53:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'ເຄື່ອງແມ່ຂ່າຍຂອງ DNS ທ້ອງຖິ່ນສາມາດນໍາໃຊ້ສໍາລັບທີ່ຢູ່ແຜນທີ່ IP ກ່ຽວກັບເຄືອຂ່າຍຂອງທ່ານທີ່ຈະຊື່ໂຮດ.';
+$lang['dns_app_name'] = 'Server DNS';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'Server DNS';
