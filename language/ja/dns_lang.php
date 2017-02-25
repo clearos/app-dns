9f3eff422584a1dd5be9356930579ad498db4d3b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:52:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'エイリアス';
+$lang['dns_app_description'] = 'ローカルDNSサーバは、ホスト名へのネットワーク上のマッピングIPアドレスに使用することができます。';
+$lang['dns_app_name'] = 'DNSサーバー';
+$lang['dns_dns_entry'] = 'DNSエントリ';
+$lang['dns_dns_server'] = 'DNSサーバー';
