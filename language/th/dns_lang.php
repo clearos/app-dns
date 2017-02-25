3fee0a0920a3c0539a12ea0e05351a16edb50d61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:58:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'นามแฝง';
+$lang['dns_app_description'] = 'เซิร์ฟเวอร์ DNS ท้องถิ่นสามารถใช้สำหรับการทำแผนที่ที่อยู่ IP บนเครือข่ายของคุณชื่อโฮสต์';
+$lang['dns_app_name'] = 'เซิร์ฟเวอร์ DNS';
+$lang['dns_dns_entry'] = 'DNS รายการ';
+$lang['dns_dns_server'] = 'เซิร์ฟเวอร์ DNS';
