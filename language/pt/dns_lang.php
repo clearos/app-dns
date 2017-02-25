b2fc8b31c53cb90ffbd4c9e4b50eea8a404d6806
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:56:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'aliás';
+$lang['dns_app_description'] = 'O servidor DNS local pode ser usado para mapeamento de endereços IP em sua rede para nomes de host.';
+$lang['dns_app_name'] = 'Servidor DNS';
+$lang['dns_dns_entry'] = 'Entrada DNS';
+$lang['dns_dns_server'] = 'Servidor DNS';
