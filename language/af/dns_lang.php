0c4907e72606793a05763407e141dcce5f31bf26
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:46:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Die plaaslike DNS-bediener gebruik kan word vir die kartering IP adresse op jou netwerk te gasheername.';
+$lang['dns_app_name'] = 'DNS-bediener';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS-bediener';
