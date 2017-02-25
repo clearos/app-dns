a557dff2e33b7e27ba3b399cc1fe624f69c28cf0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:57:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'The server DNS maxaliga ah ee waxaa loo isticmaali karaa cinwaanada IP khariidaynta aad network si hostnames.';
+$lang['dns_app_name'] = 'Server DNS';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'Server DNS';
