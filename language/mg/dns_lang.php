a6b80633dd4519aa3ded3df293dadb500e36dec0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:53:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'antsoina';
+$lang['dns_app_description'] = 'Ny eo an-toerana mpizara DNS azo ampiasaina ho an&#39;ny tsarintany IP adiresy eo amin&#39;ny tambajotra ho hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
