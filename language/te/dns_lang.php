7242fb790aa76ec39af725cfb16a39ce48354d11
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:58:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'అలియాస్';
+$lang['dns_app_description'] = 'స్థానిక DNS సర్వర్ అతిథేయినామాలను మీ నెట్వర్క్లో మ్యాపింగ్ IP చిరునామాల కొరకు ఉపయోగించవచ్చు.';
+$lang['dns_app_name'] = 'DNS సర్వర్';
+$lang['dns_dns_entry'] = 'DNS ఎంట్రీ';
+$lang['dns_dns_server'] = 'DNS సర్వర్';
