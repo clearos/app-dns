70108ab0cf77d956df0235d96265fe0d661bf894
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:50:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ઉપનામ';
+$lang['dns_app_description'] = 'સ્થાનિક DNS સર્વર હોસ્ટનેમ કરવા માટે તમારા નેટવર્ક પર મેપિંગ IP સરનામાઓ માટે વાપરી શકાય છે.';
+$lang['dns_app_name'] = 'DNS સર્વર';
+$lang['dns_dns_entry'] = 'DNS એન્ટ્રી';
+$lang['dns_dns_server'] = 'DNS સર્વર';
