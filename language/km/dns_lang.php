3a19dbb91c4ae85015e2566d97dfa8b93ccb6d56
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:52:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ឈ្មោះហៅក្រៅ';
+$lang['dns_app_description'] = 'ម៉ាស៊ីនបម្រើ DNS មូលដ្ឋានអាចត្រូវបានប្រើសម្រាប់ការធ្វើផែនទីនៅលើអាសយដ្ឋាន IP របស់អ្នកទៅម៉ាស៊ីនបណ្តាញ។';
+$lang['dns_app_name'] = 'ម៉ាស៊ីនបម្រើ DNS';
+$lang['dns_dns_entry'] = 'ធាតុ DNS';
+$lang['dns_dns_server'] = 'ម៉ាស៊ីនបម្រើ DNS';
