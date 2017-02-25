30f79ee69f6093124c81cb9edcd320e507d79109
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:51:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'utu aha';
+$lang['dns_app_description'] = 'The mpaghara DNS nkesa ga-eji na nkewa adreesị IP na netwọk gị na-hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
