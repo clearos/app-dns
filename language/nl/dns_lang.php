3a74ce39245a5f896b15cc9a8f3924907488f251
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:55:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'De lokale DNS-server kan worden gebruikt voor het toewijzen van IP-adressen in uw netwerk naar hostnames.';
+$lang['dns_app_name'] = 'DNS-server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS-server';
