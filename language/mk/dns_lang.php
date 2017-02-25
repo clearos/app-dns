2b5e428d127e609435946061f603e0641c93cf2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:54:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'алијас';
+$lang['dns_app_description'] = 'локалните DNS сервер може да се користи за мапирање на IP адреси на вашата мрежа во име на домаќин.';
+$lang['dns_app_name'] = 'DNS сервер';
+$lang['dns_dns_entry'] = 'DNS влез';
+$lang['dns_dns_server'] = 'DNS сервер';
