7150fb5658bf2db199d0f70642f04afa36841fe8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:56:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Serverul DNS local poate fi utilizat pentru maparea adreselor IP din rețea la nume de gazde.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS intrare';
+$lang['dns_dns_server'] = 'DNS Server';
