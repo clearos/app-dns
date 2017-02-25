e1f95611c96fc9db46bfa0889a1657bc18e68f8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:59:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Gall y gweinydd DNS lleol yn cael ei ddefnyddio ar gyfer cyfeiriadau mapio IP ar eich rhwydwaith i enwau gwesteiwyr.';
+$lang['dns_app_name'] = 'Gweinydd DNS';
+$lang['dns_dns_entry'] = 'DNS Mynediad';
+$lang['dns_dns_server'] = 'Gweinydd DNS';
