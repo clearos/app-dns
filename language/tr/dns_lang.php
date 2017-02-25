f2610cce2338583f4f92d0011ff8935675ad25ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:59:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'takma ad';
+$lang['dns_app_description'] = 'Yerel DNS sunucusu konak ağınızdaki haritalama IP adresleri için de kullanılabilir.';
+$lang['dns_app_name'] = 'DNS Sunucusu';
+$lang['dns_dns_entry'] = 'DNS kayıt';
+$lang['dns_dns_server'] = 'DNS Sunucusu';
