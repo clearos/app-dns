894193cb0aab3df7218ad56553c16bd046006ee1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:48:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = '别号';
+$lang['dns_app_description'] = '本地DNS服务器可用于IP地址映射您的网络上的主机名。';
+$lang['dns_app_name'] = 'DNS服务器';
+$lang['dns_dns_entry'] = 'DNS条目';
+$lang['dns_dns_server'] = 'DNS服务器';
