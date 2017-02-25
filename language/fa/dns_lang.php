1b90cb33f7878eb4489fa738badc4992dd7a8d7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:55:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'نام مستعار';
+$lang['dns_app_description'] = 'سرور DNS محلی را می توان برای آدرس IP نقشه برداری در شبکه خود را به اسامی گره استفاده می شود.';
+$lang['dns_app_name'] = 'دی ان اس سرور';
+$lang['dns_dns_entry'] = 'ورودی DNS';
+$lang['dns_dns_server'] = 'دی ان اس سرور';
