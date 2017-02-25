f719d8874039f72c8fbd1ccc3a6ab93020bcf52c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:54:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'അപരാഭിധാനം';
+$lang['dns_app_description'] = 'പ്രാദേശിക DNS സെർവർ ഹോസ്റ്റ്നെയിമുകൾ നിങ്ങളുടെ നെറ്റ്വർക്കിൽ മാപ്പിംഗ് ഐപി വിലാസങ്ങൾ ഉപയോഗിക്കാൻ കഴിയും.';
+$lang['dns_app_name'] = 'DNS സെർവർ';
+$lang['dns_dns_entry'] = 'ഡിഎൻഎസ് എൻട്രി';
+$lang['dns_dns_server'] = 'DNS സെർവർ';
