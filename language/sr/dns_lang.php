c045d21d7e7571b8023de888d407e3ea796ab6cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:56:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'псеудоним';
+$lang['dns_app_description'] = 'Локални ДНС сервер може да се користи за мапирање ИП адреса у мрежи до имена хостова.';
+$lang['dns_app_name'] = 'ДНС сервер';
+$lang['dns_dns_entry'] = 'ДНС Ступање';
+$lang['dns_dns_server'] = 'ДНС сервер';
