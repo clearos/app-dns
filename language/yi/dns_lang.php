1ea4b7da876e5d1cbd64237ca2084910f43d35d0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:00:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'אַליאַס';
+$lang['dns_app_description'] = 'די היגע דנס סערווער קענען ווערן געניצט פֿאַר מאַפּינג IP ווענדט אויף אייער נעץ צו האָסטנאַמעס.';
+$lang['dns_app_name'] = 'דנס סערווירער';
+$lang['dns_dns_entry'] = 'דנס ענטרי';
+$lang['dns_dns_server'] = 'דנס סערווירער';
