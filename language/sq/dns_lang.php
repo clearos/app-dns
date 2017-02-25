6a1eb73e5f05a981fc9422e6ad6d6fa3d6b89687
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:46:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'pseudonim';
+$lang['dns_app_description'] = 'Server DNS lokale mund të përdoret për adresat IP mapping në rrjetin tuaj në hostname.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Hyrja';
+$lang['dns_dns_server'] = 'DNS Server';
