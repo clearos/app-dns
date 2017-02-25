982cca27b8ddaa8966ddec842f3cc389b820b91a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:52:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'ადგილობრივი DNS სერვერზე შეიძლება გამოყენებულ იქნას რუკების IP მისამართები თქვენი ქსელის ჰოსტის.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS ჩანაწერი';
+$lang['dns_dns_server'] = 'DNS Server';
