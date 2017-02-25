de71a75de4a850448ed43c67475015a62ce5cec0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:58:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'என்கிற';
+$lang['dns_app_description'] = 'உள்ளூர் DNS சேவையகம் புரவலன் உங்கள் நெட்வொர்க்கில் மேப்பிங் ஐபி முகவரிகள் பயன்படுத்த முடியும்.';
+$lang['dns_app_name'] = 'DNS சேவையகம்';
+$lang['dns_dns_entry'] = 'DNS நுழைவு';
+$lang['dns_dns_server'] = 'DNS சேவையகம்';
