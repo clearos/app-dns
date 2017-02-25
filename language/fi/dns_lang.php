15f88ae7d68ba15a677fa8eeb3b79f144a635409
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:49:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Paikallinen DNS-palvelin voidaan käyttää kartoituksen IP-osoitteita verkossa Isännän.';
+$lang['dns_app_name'] = 'DNS-palvelin';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS-palvelin';
