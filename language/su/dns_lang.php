287045c1eac0ae283997e03bf5df8b549ceb5305
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:57:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Pangladén DNS lokal bisa dipaké pikeun Alamat pemetaan IP dina jaringan Anjeun pikeun hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
