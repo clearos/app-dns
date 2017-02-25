14fd43a833d8e1b4a062ab71487d632618b523f2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:53:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'ingoakē';
+$lang['dns_app_description'] = 'Ka taea te whakamahi i te tūmau DNS rohe mō wāhitau mahere IP i runga i tou whatunga ki hostnames.';
+$lang['dns_app_name'] = 'Tūmau DNS';
+$lang['dns_dns_entry'] = 'Tāurunga DNS';
+$lang['dns_dns_server'] = 'Tūmau DNS';
