040d4a03e60a3095e51d0eda7b63a3c52484baff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:59:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'псевдонім';
+$lang['dns_app_description'] = 'Локальний сервер DNS може бути використаний для відображення IP-адрес в мережі імена хостів.';
+$lang['dns_app_name'] = 'DNS-сервер';
+$lang['dns_dns_entry'] = 'DNS-запис';
+$lang['dns_dns_server'] = 'DNS-сервер';
