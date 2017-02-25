4ca02bcb571bec9fa0c7907135abd6b353d8736c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:57:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'ndani DNS server inaweza kutumika kwa anwani ramani IP kwenye mtandao wako kwa hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
