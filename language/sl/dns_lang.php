dff831f8a500c93f48cdd34de7eeb6fcc56a2262
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:57:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'Lokalni strežnik DNS se lahko uporablja za preslikavo naslovov IP v omrežju do gostiteljskih imen.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Začetek';
+$lang['dns_dns_server'] = 'DNS Server';
