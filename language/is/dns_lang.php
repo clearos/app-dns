f60584261d86ac8ba7aaeb22d4926877a1f73a9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:51:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'samnefni';
+$lang['dns_app_description'] = 'Sveitarstjórn DNS framreiðslumaður geta vera notaður fyrir kortlagning IP-tölur í netið til vélanafna.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
