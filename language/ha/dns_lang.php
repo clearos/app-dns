4d3394a45a6bc509a2f8fa4180d20cddf84dfb54
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:50:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'wanda aka ce masa';
+$lang['dns_app_description'] = 'The gida DNS uwar garken za a iya amfani da linzaman taswira IP adiresoshin a kan hanyar sadarwa zuwa hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
