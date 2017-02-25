2a83ac29895ee0d419408d8c9a65255401000260
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:56:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'නොහොත්';
+$lang['dns_app_description'] = 'දේශීය DNS සේවාදායකයේ ධාරක ඔබේ ජාලය මත සිතියම් ගත IP ලිපිනයන් සඳහා යොදා ගත හැකිය.';
+$lang['dns_app_name'] = 'DNS සේවාදායක';
+$lang['dns_dns_entry'] = 'DNS සටහන්';
+$lang['dns_dns_server'] = 'DNS සේවාදායක';
