5061cc00ddc8199688f7b8946943aa8cf6921022
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:53:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'alias';
+$lang['dns_app_description'] = 'DNS moderatruo Loci IP oratio potest pro mapping in vestri network ut hostnames.';
+$lang['dns_app_name'] = 'DNS Server';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS Server';
