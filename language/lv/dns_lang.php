93578deba2d88bc3a291e78bb80cbe3806f6ebd5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:53:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'citādi';
+$lang['dns_app_description'] = 'Vietējā DNS serveri var izmantot kartēšana IP adresēm uz jūsu tīklā mītņu.';
+$lang['dns_app_name'] = 'DNS serveris';
+$lang['dns_dns_entry'] = 'DNS Entry';
+$lang['dns_dns_server'] = 'DNS serveris';
