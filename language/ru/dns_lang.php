445fc81d129ffbd492e3d1d5c59192d47919eff3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:56:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'псевдоним';
+$lang['dns_app_description'] = 'Локальный сервер DNS может быть использован для отображения IP-адресов в сети имена хостов.';
+$lang['dns_app_name'] = 'DNS-сервер';
+$lang['dns_dns_entry'] = 'DNS-запись';
+$lang['dns_dns_server'] = 'DNS-сервер';
