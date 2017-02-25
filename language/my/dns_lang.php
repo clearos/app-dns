7b55a497812cd006314b49fb8dde3b7abadd969b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 01:48:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/dns_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['dns_alias'] = 'အမည်ဝှက်';
+$lang['dns_app_description'] = 'အဆိုပါဒေသခံ DNS Server ကို hostnames ရန်သင့် network ပေါ်မှာမြေပုံ IP လိပ်စာများအတွက်အသုံးပြုနိုင်ပါတယ်။';
+$lang['dns_app_name'] = 'DNS ကို Server ကို';
+$lang['dns_dns_entry'] = 'DNS ကို Entry &#39;';
+$lang['dns_dns_server'] = 'DNS ကို Server ကို';
